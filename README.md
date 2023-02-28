335bb6d84e9cffdc20a413aed1b0d03ef524a534
Big Trees <traveye@protonmail.com>
2023-02-28 11:21:05 -0800

    adding depends, adding prelim scritps, adding output and plugins

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # pwa
+
+### Table of Contents
+* [Goals](#goals)
+* [Implementation](#implementation)
+* [Credits](#credits)
+* [Usage](#usage)
+* [License](#licene)
+
+## Goals
+
+
+
+## Implementation
+
+To build this site, I used the following technologies:
+- Express
+
+
+
+## Credits
+N/A
+
+## Usage
+N/A
+
+## License
+MIT
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -11,11 +14,30 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
     },
 
     // TODO: Add the correct plugins
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new GenerateSW({}),
+      new WebpackPwaManifest({
+        name: "TODO's List",
+        short_name: "TODOs",
+        description: "A simple TODO list app",
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#2196f3',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
      
     ],
 
